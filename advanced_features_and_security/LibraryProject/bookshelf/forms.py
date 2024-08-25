63b63c3f4f086672b4be405edd024c74ca4c02ac
Ermi539 <ermiashabtamu477@gmail.com>
2024-08-25 04:45:22 -0700

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,2 +1,34 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+ExampleForm
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date', 'isbn']
+        widgets = {
+            'published_date': forms.DateInput(attrs={'type': 'date'}),
+            'isbn': forms.TextInput(attrs={'placeholder': 'ISBN Number'}),
+        }
+
+    def clean_isbn(self):
+        """Custom validation for ISBN."""
+        isbn = self.cleaned_data.get('isbn')
+        if not isbn:
+            raise forms.ValidationError('ISBN is required.')
+        if len(isbn) != 13:
+            raise forms.ValidationError('ISBN must be exactly 13 digits.')
+        # Add more validation as needed
+        return isbn
+
+    def clean_title(self):
+        """Custom validation for Title."""
+        title = self.cleaned_data.get('title')
+        if not title:
+            raise forms.ValidationError('Title cannot be empty.')
+        return title
+
+    def clean(self):
+        """General validation."""
+        cleaned_data = super().clean()
+   
