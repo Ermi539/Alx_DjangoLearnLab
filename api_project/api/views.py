a64606e8c95fd2ccd852c77f224378f787d571b6
Ermi539 <ermiashabtamu477@gmail.com>
2024-08-31 00:59:06 -0700

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -38,6 +38,7 @@ class BookViewSet(viewsets.ModelViewSet):
        queryset = Book.objects.all()
        serializer_class = BookSerializer
        permission_classes = [IsAuthenticated]
+rest_framework.permissions.IsAuthenticated
    
 
 
