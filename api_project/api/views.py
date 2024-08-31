be558310e447eb942cf1868c56faa60f3cae944b
Ermi539 <ermiashabtamu477@gmail.com>
2024-08-31 00:51:03 -0700

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -10,3 +10,11 @@ class BookList(APIView):
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 generics.ListAPIView
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
