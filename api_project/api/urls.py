04b3a0e84e4d545dc49d70e2f8c8355985bb8825
Ermi539 <ermiashabtamu477@gmail.com>
2024-08-31 00:51:49 -0700

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from rest_framework import routers
+from . import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include the 'api' app's URLs
-]
+router = routers.DefaultRouter()
+router.register(r'books', views.BookViewSet)
+
+urlpatterns = router.urls
