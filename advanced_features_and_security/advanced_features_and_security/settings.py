ad6471ef5b52e6a385cd9d98aedc056862dde35d
Ermi539 <ermiashabtamu477@gmail.com>
2024-09-08 00:55:03 -0700

    Update settings.py

--- advanced_features_and_security/advanced_features_and_security/settings.py
+++ advanced_features_and_security/advanced_features_and_security/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aip',
+    'rest_framework',
+    'api', 
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,11 @@ WSGI_APPLICATION = 'advanced_features_and_security.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': BASE_DIR / 'db.sqlite3',
+       }
+   }
 
 
 # Password validation
