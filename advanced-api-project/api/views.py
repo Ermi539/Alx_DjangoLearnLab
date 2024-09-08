72881b8e86e9df29a821b28fc5ec89f35b28d7c0
Ermi539 <ermiashabtamu477@gmail.com>
2024-09-08 04:44:00 -0700

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -11,3 +11,12 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+# api/views.py
+
+class BookListView(generics.ListCreateAPIView):
+    # ... (other code) 
+    def create(self, request, *args, **kwargs):
+        # You can add custom logic here before or after the standard creation process
+        response = super().create(request, *args, **kwargs)
+        # ... (additional logic if needed)
+        return response
