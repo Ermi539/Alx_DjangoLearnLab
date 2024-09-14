694b1e9dbf56be66aa2efbf2ec30712abf3d5ce4
Ermi539 <ermiashabtamu477@gmail.com>
2024-09-14 09:29:18 -0700

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,3 +7,10 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+]
